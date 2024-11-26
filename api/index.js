cebfb4b14c2525136fdb47fa26ae3f3623e9f0d7
mrckd1605 <mrckd1605@gmail.com>
2024-11-26 22:15:09 +0530

    create an api for testing

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 const app = express()
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 
 dotenv.config()
 
@@ -10,8 +11,15 @@ mongoose.connect(process.env.MONGOCONNECTION)
 .then(()=>{
     console.log("mongodb is connected")
 }).catch(err=>{
-    console.log(err)
+    console.log("Mongodb arror is : ",err)
 })
+
+app.use('/api/user',userRouter)
+
+
+
+
 app.listen(3000,()=>{
     console.log("server listening at 3000")
 })
+
