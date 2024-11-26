54df0346f8e202fa9c59c51014f3eddbad409705
mrckd1605 <mrckd1605@gmail.com>
2024-11-26 19:43:36 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
 const app = express()
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGOCONNECTION)
+.then(()=>{
+    console.log("mongodb is connected")
+}).catch(err=>{
+    console.log(err)
+})
 app.listen(3000,()=>{
     console.log("server listening at 3000")
 })
