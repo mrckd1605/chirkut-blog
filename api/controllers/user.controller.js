3255d4cd06d5638982dbee32708bfceacf39ead3
mrckd1605 <mrckd1605@gmail.com>
2024-12-05 12:55:38 +0530

    update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,66 @@
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from 'bcryptjs'
+import User from "../models/user.model.js"
+
 export const test = (req,res)=>{
     res.json({massage:"api is working"})
 }
+
+
+export const updateUser = async (req, res, next) => {
+    try {
+      
+        if (req.user.id !== req.params.userId) {
+            return next(errorHandler(403, "You are not allowed to update this user."));
+        }
+
+ 
+        if (req.body.password) {
+            if (req.body.password.length < 6) {
+                return next(errorHandler(400, "Password should be at least 6 characters long."));
+            }
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+   
+        if (req.body.username) {
+            const username = req.body.username;
+            if (username.length < 6 || username.length > 20) {
+                return next(errorHandler(400, "Username should be between 6 and 20 characters."));
+            }
+            if (username.includes(" ")) {
+                return next(errorHandler(400, "Username cannot contain spaces."));
+            }
+            if (username !== username.toLowerCase()) {
+                return next(errorHandler(400, "Username must be in lowercase."));
+            }
+            if (!/^[a-z0-9]+$/.test(username)) { // Fixed logic
+                return next(errorHandler(400, "Username can only contain lowercase letters and numbers."));
+            }
+        }
+
+    
+        const updates = {
+            username: req.body.username,
+            email: req.body.email,
+            profilePicture: req.body.profilePicture,
+            ...(req.body.password && { password: req.body.password }) // Include password only if it exists
+        };
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.userId,
+            { $set: updates },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return next(errorHandler(404, "User not found."));
+        }
+
+   
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+
 
 dotenv.config()
 
@@ -15,6 +17,7 @@ mongoose.connect(process.env.localMongoUri,{ autoIndex: true })
 })
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
