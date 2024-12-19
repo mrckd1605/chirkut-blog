f26817d37f084579a1203ea54866a0d53ae196d2
mrckd1605 <mrckd1605@gmail.com>
2024-12-19 12:43:31 +0530

    complete the functionality of update user profile

--- chirkut-blog/src/components/DashProfile.jsx
+++ chirkut-blog/src/components/DashProfile.jsx
@@ -1,14 +1,17 @@
-import { Button, TextInput } from 'flowbite-react'
+import { Alert, Button, TextInput } from 'flowbite-react'
 import React, { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
-
-
+import { updateStart,updateSuccess,updateFailure } from '../redux/user/userSlice'
+import { useDispatch, useSelector } from 'react-redux' 
 
 export default function DashProfile() {
     const filePickerRef = useRef()
     const [imageFile,setImageFile] = useState(null)
     const [imageFileUrl,setImageFileUrl] = useState(null)
     const { currentUser } = useSelector(state => state.user)
+    const [formData,setFormData] = useState({})
+    const [updateUserSuccess,setUpdateUserSuccess] = useState(null)
+    const [updateUserError,setUpdateUserError] = useState(null)
+    const dispatch = useDispatch()
     const handleImageChange=(e)=>{
         const file = e.target.files[0]
         if(file){
@@ -25,10 +28,45 @@ export default function DashProfile() {
     const uploadImage = async ()=>{
         console.log("uploading image")
     }
+
+
+    const handleChange = (e)=>{
+        setFormData({...formData,[e.target.id]:e.target.value})
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        setUpdateUserError(null)
+        setUpdateUserSuccess(null)
+        if(Object.keys(formData).length === 0){
+            setUpdateUserError('No changes made')
+            return
+        }
+        try{
+            dispatch(updateStart())
+            const res = await fetch(`/api/user/update/${currentUser._id}`,{
+                method: 'PUT',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify(formData)
+            })
+            const data = await res.json()
+            if(!res.ok){
+                dispatch(updateFailure(data.message))
+                setUpdateUserError(data.message)
+            }else{
+                dispatch(updateSuccess(data))
+                setUpdateUserSuccess('User profile update successfully')
+            }
+        }catch(error){
+            dispatch(updateFailure(error.message))
+            setUpdateUserError(error.message)
+        }
+    }
+
     return (
         <div className='max-w-lg mx-auto p-3 w-full'>
             <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-            <form className='flex flex-col gap-6'>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
             <input type='file' accept='image/*' 
             onChange={handleImageChange} ref={filePickerRef} hidden/>
                 <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
@@ -37,15 +75,35 @@ export default function DashProfile() {
                         className='rounded-full w-full h-full border-8 object-cover border-[lightgray]'>
                     </img>
                 </div>
-                <TextInput type='text' placeholder='username' id='username' defaultValue={currentUser.username}/>
-                <TextInput type='email' placeholder='email' id='email' defaultValue={currentUser.email}/>
-                <TextInput type='password' placeholder='password' id='password'/>
+                <TextInput type='text' placeholder='username' id='username' defaultValue={currentUser.username}
+                    onChange={handleChange}
+                />
+                <TextInput type='email' placeholder='email' id='email' defaultValue={currentUser.email}
+                     onChange={handleChange}
+                />
+                <TextInput type='password' placeholder='password' id='password'
+                     onChange={handleChange}
+                />
                 <Button type='submit' gradientDuoTone='purpleToBlue' outline>Update</Button>
              </form>
              <div className='text-red-500 flex justify-between mt-5'>
                 <span className='cursor-pointer'>Delete Acount</span>
                 <span className='cursor-pointer'>Sign Out</span>
              </div>
+             {
+                updateUserSuccess && (
+                    <Alert color='success' className='mt-5'>
+                        {updateUserSuccess}
+                    </Alert>
+                )
+             }
+             {
+                updateUserError && (
+                    <Alert color='failure' className='mt-5'>
+                        {updateUserError}
+                    </Alert>
+                )
+             }
         </div>
     )
 }
