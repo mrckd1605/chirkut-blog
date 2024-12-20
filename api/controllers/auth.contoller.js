7a43e7425fc02a44453097771076e8af12729c1a
mrckd1605 <mrckd1605@gmail.com>
2024-12-20 10:41:58 +0530

    add delete api routes for userProfile

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -37,12 +37,12 @@ export const signIn = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, 'Invalid credentials'));
+      return next(errorHandler(404, 'User not found'));
     }
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(errorHandler(404, 'Invalid credentials'));
+      return next(errorHandler(404, 'user not found'));
     }
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
